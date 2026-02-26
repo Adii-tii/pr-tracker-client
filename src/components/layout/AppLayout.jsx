a93b8d577e075c37ec23d6ace318438aa50ac240
Adii-tii <sableaditi8@gmail.com>
2026-02-26 10:51:25 +0530

    - repo switch option in sidebar now - logout functionality added

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -2,20 +2,20 @@ import Header from "./Header";
 import { Sidebar } from "./Sidebar";
 import { Outlet } from "react-router-dom";
 
-function AppLayout() {
+function AppLayout({ user }) {
   return (
     <div className="flex h-screen bg-bg">
       {/* Sidebar */}
-      <Sidebar />
+      <Sidebar user={user} />
 
       {/* Right side (header + content) */}
       <div className="flex flex-1 flex-col overflow-hidden">
         {/* Header */}
-        <Header />
+        <Header user={user} />
 
         {/* Main Content */}
         <main className="flex-1 overflow-y-auto bg-bg">
-          <div className="mx-auto max-w-auto px-6">
+          <div className="mx-auto max-w-auto px-6 pb-6">
             <Outlet />
           </div>
         </main>
